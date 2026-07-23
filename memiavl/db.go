e59bba743563263ecb5787469899b55c2e73caab
jay.tseng <jay.tseng@crypto.com>
2026-07-23 13:38:48 -0400

    refine comments

--- memiavl/db.go
+++ memiavl/db.go
@@ -532,7 +532,7 @@ func (db *DB) checkBackgroundSnapshotRewrite() error {
 			time.Sleep(time.Nanosecond)
 		}
 
-		// on failure, close the tree to release its mmap'd snapshot files before returning.
+		// catchup the remaining wal
 		if err := result.mtree.CatchupWAL(db.wal, 0); err != nil {
 			return errors.Join(fmt.Errorf("final catchup failed: %w", err), result.mtree.Close())
 		}
@@ -873,7 +873,6 @@ func (db *DB) rewriteSnapshotBackground() error {
 
 		// do a best effort catch-up, will do another final catch-up in main thread.
 		if err := mtree.CatchupWAL(wal, 0); err != nil {
-			// close the tree here too, otherwise its mmap'd snapshot files leak.
 			ch <- snapshotResult{err: errors.Join(err, mtree.Close())}
 			return
 		}
